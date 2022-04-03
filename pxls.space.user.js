1595521728aa6f9d51165d22a603957235fb6d3d
mare077 <102875753+mare077@users.noreply.github.com>
2022-04-04 02:48:45 +0700

    Update pxls.space.user.js

--- pxls.space.user.js
+++ pxls.space.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pxls.space
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @description  Spread the love
 // @author       oralekin
 // @match        https://hot-potato.reddit.com/embed*
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/ULTFMCJ.png";
+            i.src = "https://i.imgur.com/HeEGUY0.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
