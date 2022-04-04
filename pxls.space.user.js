b08ed5dedf41596334c5e7db2fd10f0bf71dd87a
mare077 <102875753+mare077@users.noreply.github.com>
2022-04-04 15:35:05 +0700

    Update pxls.space.user.js

--- pxls.space.user.js
+++ pxls.space.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/6GJ3V7E.png";
+            i.src = "https://i.imgur.com/3kDFWE7.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
